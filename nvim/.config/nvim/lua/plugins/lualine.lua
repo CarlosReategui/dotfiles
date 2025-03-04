a4bf3ad918aa12846a3a270644271493bc037bd3
Carlos Reategui <carlos.reategui@utec.edu.pe>
2025-03-04 09:22:32 -0500

    feat(nvim): Add lualine settings for now theme.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,30 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "auto",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"linrongbin16/lsp-progress.nvim",
+	},
+	opts = {
+		options = {
+			theme = "ayu_light",
+		},
+	},
+	sections = {
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- Displays file status (readonly status, modified status)
+				newfile_status = false, -- Display new file status (new file means no write after created)
+				path = 4, -- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
+				-- 3: Absolute path, with tilde as the home directory
+				-- 4: Filename and parent dir, with tilde as the home directory
+				symbols = {
+					modified = "[+]", -- Text to show when the file is modified.
+					readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+				},
 			},
-		})
-	end,
+		},
+	},
 }
