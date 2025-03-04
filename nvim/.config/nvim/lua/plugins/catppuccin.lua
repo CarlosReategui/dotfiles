74f8cab987fa31ce173a9efea51ab7bb4f197908
Carlos Reategui <carlos.reategui@utec.edu.pe>
2025-03-04 09:19:54 -0500

    feat(nvim): Update theme options.

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,10 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      transparent_background = true,
-    })
-    vim.cmd.colorscheme("catppuccin-latte")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin-latte")
+	end,
 }
