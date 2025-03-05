12488620c40d72f832beece3b23bde1076ffda6d
Carlos Reategui <carlos.reategui@utec.edu.pe>
2025-03-04 21:21:43 -0500

    feat(nvim): Add fzf to telescope.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,37 @@
 return {
   {
+    -- https://github.com/nvim-telescope/telescope.nvim
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>l", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    lazy = true,
+    dependencies = {
+      -- https://github.com/nvim-lua/plenary.nvim
+      { "nvim-lua/plenary.nvim" },
+      {
+        -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+    },
+    opts = {
+      defaults = {
+        layout_config = {
+          vertical = {
+            width = 0.75,
+          },
+        },
+        path_display = {
+          filename_first = {
+            reverse_directories = true,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
     end,
   },
   {
