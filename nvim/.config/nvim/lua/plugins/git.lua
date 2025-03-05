0e440a5625b419c55cb6e581d3416ae194d174d0
Carlos Reategui <carlos.reategui@utec.edu.pe>
2025-03-04 21:19:46 -0500

    feat(nvim): Refactor git file.

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -4,13 +4,8 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        current_line_blame = true,
-      })
-
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end,
+    opts = {
+      current_line_blame = true,
+    },
   },
 }
