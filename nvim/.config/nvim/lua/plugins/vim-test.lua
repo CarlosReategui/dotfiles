3e3a50166e268ac4b7f714eb96b8e2a3343960e1
Carlos Reategui <carlos.reategui@utec.edu.pe>
2025-03-07 10:28:48 -0500

    feat(nvim): Temporarily disable vim-test.

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -4,11 +4,11 @@ return {
 		"preservim/vimux",
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>", {})
-		vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", {})
-		vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", {})
-		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
-		vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>", {})
+		-- vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>", {})
+		-- vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", {})
+		-- vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", {})
+		-- vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
+		-- vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>", {})
 		vim.cmd("let test#strategy = 'vimux'")
 	end,
 }
