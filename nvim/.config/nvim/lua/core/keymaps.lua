9c6168eb1fd7d2d75f8000ad3df1b3cd6738fc18
Carlos Reategui <carlos.reategui@utec.edu.pe>
2025-03-10 21:44:13 -0500

    feat(nvim): Change split window management mapping.

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -21,14 +21,14 @@ keymap.set("n", "<c-h>", ":wincmd h<CR>")
 keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Split window management
-keymap.set("n", "<leader>sv", "<C-w>v")     -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s")     -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=")     -- make split windows equal width
-keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
-keymap.set("n", "<leader>sj", "<C-w>-")     -- make split window height shorter
-keymap.set("n", "<leader>sk", "<C-w>+")     -- make split windows height taller
-keymap.set("n", "<leader>sl", "<C-w>>5")    -- make split windows width bigger
-keymap.set("n", "<leader>sh", "<C-w><5")    -- make split windows width smaller
+keymap.set("n", "<leader>wv", "<C-w>v")     -- split window vertically
+keymap.set("n", "<leader>wh", "<C-w>s")     -- split window horizontally
+keymap.set("n", "<leader>we", "<C-w>=")     -- make split windows equal width
+keymap.set("n", "<leader>wx", ":close<CR>") -- close split window
+keymap.set("n", "<leader>wj", "<C-w>-")     -- make split window height shorter
+keymap.set("n", "<leader>wk", "<C-w>+")     -- make split windows height taller
+keymap.set("n", "<leader>wl", "<C-w>>5")    -- make split windows width bigger
+keymap.set("n", "<leader>wh", "<C-w><5")    -- make split windows width smaller
 
 -- Tab management
 keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open a new tab
